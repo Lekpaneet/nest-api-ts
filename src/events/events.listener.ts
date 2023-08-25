fcfaf97efeac873d1ab0f82aecac1bc2b557823f
ponlawat koeisuwan <ponlawat6001@gmail.com>
2023-08-25 14:32:13 +0700

    update event

--- src/events/events.listener.ts
+++ src/events/events.listener.ts
@@ -6,8 +6,10 @@ export class EventsListener {
 
     @OnEvent('event.action')
     handleActionEvent(payload: any): void{
+        const timeJob = Math.abs((Math.random()*5) *1000);
+        const now = Date.now();
         setTimeout(() => {
-            console.log(`On Event - `, payload);
-        }, 5000);
+            console.log(`On Event ${(Date.now()-now)/1000}s - `, payload);
+        }, timeJob);
     }
 }
