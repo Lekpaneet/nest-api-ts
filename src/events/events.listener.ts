bbd7c52681a7f7a1b8ec829341887706b2ba62e8
ponlawat koeisuwan <ponlawat6001@gmail.com>
2023-08-25 14:38:49 +0700

    Update events.listener.ts

--- src/events/events.listener.ts
+++ src/events/events.listener.ts
@@ -3,7 +3,6 @@ import { OnEvent } from "@nestjs/event-emitter";
 
 @Injectable()
 export class EventsListener {
-
     @OnEvent('event.action')
     handleActionEvent(payload: any): void{
         const timeJob = Math.abs((Math.random()*5) *1000);
